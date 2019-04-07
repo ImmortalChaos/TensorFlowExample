05f5c5cc77d29bec84c6121da576568f5fe1693f
park minho <>
2019-04-07 22:30:28 +0900

    #17 append marking option

--- example-011/face_detection.py
+++ example-011/face_detection.py
@@ -2,7 +2,7 @@ import os
 import argparse
 import cv2
 
-def faceDetection(imgPath) :
+def faceDetection(imgPath, showMarking) :
 	# Git : https://github.com/opencv/opencv/tree/master/data/haarcascades
 	face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 	eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
@@ -11,12 +11,14 @@ def faceDetection(imgPath) :
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 	for (x,y,w,h) in faces:
-	    img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+	    if showMarking :
+	        img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
 	    roi_gray = gray[y:y+h, x:x+w]
 	    roi_color = img[y:y+h, x:x+w]
 	    eyes = eye_cascade.detectMultiScale(roi_gray)
 	    for (ex,ey,ew,eh) in eyes:
-	        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+	        if showMarking :
+	            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
 	cv2.imshow('img',img)
 	cv2.waitKey(0)
@@ -26,9 +28,11 @@ if __name__=="__main__":
 	parser = argparse.ArgumentParser(description='Face Detection Program')
 	parser.add_argument('-source', type=str, required=True, default=os.getcwd(),
 			help='Appoint the source folder or file path')
+	parser.add_argument('-show_marking', default=False, action='store_true', 
+			help='Displays the found area.')
 	args = parser.parse_args()
 	
 	sourceFolder = args.source
-	faceDetection(sourceFolder)
+	faceDetection(sourceFolder, args.show_marking)
 
 
